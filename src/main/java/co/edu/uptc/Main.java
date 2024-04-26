c6808cc63fd54ab4e2fb5a628851b1de97176ca0
Jean-Dv <jean.valencia01@proton.me>
2024-04-26 10:36:02 -0500

    feat: add log4j2 for logging application into files and console output

--- src/main/java/co/edu/uptc/Main.java
+++ src/main/java/co/edu/uptc/Main.java
@@ -1,11 +1,16 @@
 package co.edu.uptc;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Main class.
  */
 public class Main {
+  private static Logger logger = LogManager.getLogger(Main.class);
+
   public static void main(String[] args) {
     int a = 10;
-    System.out.println("Hello World!" + a);
+    logger.info("Hello World!" + a);
   }
 }

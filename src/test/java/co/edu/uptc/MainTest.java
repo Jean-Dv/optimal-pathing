380eac99237249008215955e48ba73ac82856cfb
Jean-Dv <jean.valencia01@proton.me>
2024-04-26 13:10:10 -0500

    feat: add one test

---
+++ src/test/java/co/edu/uptc/MainTest.java
@@ -0,0 +1,15 @@
+package co.edu.uptc;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Test Main Test.
+ */
+public class MainTest {
+  @Test
+  public void test() {
+    assertTrue(true);
+  }
+}

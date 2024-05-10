ca22f18a7f6017656886134998726d5b0928b5bc
Jean Valencia <72704457+Jean-Dv@users.noreply.github.com>
2024-05-09 19:01:35 -0500

    PB-7 read shipping orders (#8)
    * Pb 22 feat/get all shipping orders controller (#6)
    
    * PB-22 fix: modified before to before class for testing purposes
    
    * PB-22 feat: add test for getting all orders
    
    * PB-22 feat: add getAll method to OrderController and OrderRepository
    
    * PB-23 feat: create interfaz read orders (#7)
    
    ---------
    
    Co-authored-by: Bibian Corredor <138538175+BibianCo@users.noreply.github.com>

--- src/main/java/co/edu/uptc/model/OrderRepository.java
+++ src/main/java/co/edu/uptc/model/OrderRepository.java
@@ -1,5 +1,7 @@
 package co.edu.uptc.model;
 
+import java.util.List;
+
 /**
  * Interface that represents the repository of orders.
  */
@@ -10,4 +12,11 @@ public interface OrderRepository {
    * @param order - Order to be saved.
    */
   void save(Order order);
+
+  /**
+   * Method that allows to find all the orders.
+   *
+   * @return List of orders.
+   */
+  List<Order> findAll();
 }

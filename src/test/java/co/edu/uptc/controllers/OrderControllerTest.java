d98123ec67c52ebdfb5704a79e5806a48394cb8f
Bibian Corredor <138538175+BibianCo@users.noreply.github.com>
2024-05-11 15:25:15 -0500

    PB-18 feat:create method to edit order and test (#9)

--- src/test/java/co/edu/uptc/controllers/OrderControllerTest.java
+++ src/test/java/co/edu/uptc/controllers/OrderControllerTest.java
@@ -1,6 +1,7 @@
 package co.edu.uptc.controllers;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
 
 import co.edu.uptc.controller.orders.OrderController;
@@ -45,6 +46,33 @@ public class OrderControllerTest {
       List<Order> orders = controller.getAll();
       assertEquals(1, orders.size());
       assertEquals("Calle 3 # 24 - 43", orders.get(0).getSourceAddress());
+    } catch (Exception e) {
+      fail("Error getting all orders.");
+    }
+  }
+
+  @Test
+  public void testEditOrder() {
+    try {
+      Order order = new Order(1, LocalDate.now(), LocalDate.ofYearDay(2024, 2), "Calle 3 # 24 - 43",
+          "Calle 4 # 25 - 9", "Saliendo", "Jonh Doe", "Company X", 3000, false, "", "",
+          new Responsible("Chic Elletson", "24dcd50d-a38c-42c7-888c-1c783d988fea", "(210) 5769359",
+              "bmartt0@netscape.com"));
+      // create an order
+      controller.add(order);
+
+      // edited order with destination address
+      Order orderEdit = new Order(1, LocalDate.now(), LocalDate.ofYearDay(2024, 2),
+          "Calle 3 # 24 - 43", "Calle 6 # 25 - 9", "Saliendo", "Jonh Doe", "Company X", 3000, false,
+          "", "", new Responsible("Chic Elletson", "24dcd50d-a38c-42c7-888c-1c783d988fea",
+              "(210) 5769359", "bmartt0@netscape.com"));
+
+      assertEquals(orderEdit, controller.edit(orderEdit));
+      assertEquals(orderEdit.getDestinationAddress(),
+          controller.edit(orderEdit).getDestinationAddress());
+      orderEdit.setNumberOrder(2);
+      assertNull(controller.edit(orderEdit));
+
     } catch (Exception e) {
       fail("Error getting all orders.");
     }

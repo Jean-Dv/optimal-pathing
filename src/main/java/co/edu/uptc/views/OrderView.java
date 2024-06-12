96add79028c03f567b351280be93df7f45090383
Bibian Corredor <138538175+BibianCo@users.noreply.github.com>
2024-06-11 21:48:39 -0500

    Pb 36 create view and controller to add orders (#20)
    * PB-36 feat:add order
    
    * PB-36 feat: create view controller add orders
    
    ---------
    
    Co-authored-by: Jean Valencia <72704457+Jean-Dv@users.noreply.github.com>

--- src/main/java/co/edu/uptc/views/OrderView.java
+++ src/main/java/co/edu/uptc/views/OrderView.java
@@ -5,8 +5,12 @@ import co.edu.uptc.infrastructure.MongoClientFactory;
 import co.edu.uptc.infrastructure.orders.MongoOrderRepository;
 import co.edu.uptc.model.Order;
 import co.edu.uptc.model.OrderRepository;
+import co.edu.uptc.model.Responsible;
+import co.edu.uptc.utils.ServletUtils;
 import co.edu.uptc.utils.StringUtils;
 import com.mongodb.client.MongoClient;
+import java.io.IOException;
+import java.time.LocalDate;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -22,7 +26,63 @@ import org.apache.logging.log4j.Logger;
 @WebServlet("/order")
 public class OrderView extends HttpServlet {
   private static final Logger logger = LogManager.getLogger(OrderView.class);
+  
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
 
+    String cashonDeliveryServlet = req.getParameter("isCashOn"); // on para si, y null para no
+    String destinationAddress = req.getParameter("destinationAddress");
+    String descriptionAddress = req.getParameter("descriptionAddress");
+    String remitterName = req.getParameter("remitterName");
+    String addresseeName = req.getParameter("addresseeName");
+    String price = req.getParameter("price");
+    String responsible = req.getParameter("responsible");
+    boolean cashonDelivery = false;
+
+    if (descriptionAddress == null || destinationAddress == null || remitterName == null
+        || addresseeName == null || price == null || responsible == null) {
+
+      req.setAttribute("errorMessage", "Error: All fields are required");
+      ServletUtils.forward(req, resp, "/pages/addorder.jsp");
+      return;
+    }
+
+    if (cashonDeliveryServlet != null && cashonDeliveryServlet.equals("on")) {
+      cashonDelivery = true;
+    } else {
+      cashonDelivery = false;
+    }
+
+    if (!remitterName.matches("^[A-Za-zÁÉÍÓÚáéíóúñÑ ]+$")
+        || !addresseeName.matches("^[A-Za-zÁÉÍÓÚáéíóúñÑ ]+$")) {
+      req.setAttribute("errorMessageString", "only letters");
+      ServletUtils.forward(req, resp, "/pages/addorder.jsp");
+      return;
+    }
+
+    MongoClient mongoClient =
+        MongoClientFactory.createClient("orderView", "mongodb://localhost:27017");
+    OrderRepository orderRepository = new MongoOrderRepository(mongoClient);
+    OrderController orderController = new OrderController(orderRepository);
+
+    // TODO: You must complete the spaces since you are only jarcodiating.
+
+    Order order = new Order(LocalDate.now(), LocalDate.now(), "", destinationAddress, "",
+        addresseeName, remitterName, Integer.parseInt(price), cashonDelivery, descriptionAddress,
+        "", new Responsible(responsible, "", "", ""));
+    orderController.add(order);
+
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+
+    ServletUtils.forward(req, resp, "/pages/addorder.jsp");
+
+  }
+  
   @Override
   protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
@@ -46,5 +106,4 @@ public class OrderView extends HttpServlet {
       OrderView.logger.error(error);
     }
   }
-
 }

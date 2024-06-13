da02d294b01a341e349b0f09509121e12ad592a5
Jean-Dv <jean.valencia01@proton.me>
2024-06-12 20:02:20 -0500

    fix: modify endpoint name to orders

--- src/main/java/co/edu/uptc/views/OrdersView.java
+++ src/main/java/co/edu/uptc/views/OrdersView.java
@@ -5,10 +5,10 @@ import co.edu.uptc.infrastructure.MongoClientFactory;
 import co.edu.uptc.infrastructure.orders.MongoOrderRepository;
 import co.edu.uptc.model.Order;
 import co.edu.uptc.model.OrderRepository;
+import co.edu.uptc.utils.ServletUtils;
 import com.mongodb.client.MongoClient;
 import java.io.IOException;
 import java.util.List;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,19 +19,19 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Class that represents the view for the listed orders.
  */
-@WebServlet("/vieworders")
+@WebServlet("/orders")
 public class OrdersView extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-       throws ServletException, IOException {
-  
+      throws ServletException, IOException {
+
     MongoClient mongoClient =
         MongoClientFactory.createClient("orderView", "mongodb://localhost:27017");
     OrderRepository orderRepository = new MongoOrderRepository(mongoClient);
     OrderController orderController = new OrderController(orderRepository);
-    List<Order> orders = orderController.getAll(); 
-  
+    List<Order> orders = orderController.getAll();
+
     req.setAttribute("orders", orders);
     ServletUtils.forward(req, resp, "/pages/orders.jsp");
   }

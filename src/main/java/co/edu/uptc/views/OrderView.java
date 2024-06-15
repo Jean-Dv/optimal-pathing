8d09ed3fe62cbe1134df65124f84b54bce6b5a8c
juan-Riapira <138538939+juan-Riapira@users.noreply.github.com>
2024-06-15 13:39:43 -0500

    Pb 27 feat/interface to modify shipping order (#30) (#31)
    * PB-9 feat/edit only status in orders (#28)
    
    * PB-26 feat/add enum class to status order (#23)
    
    * feat: add Status enum for order status representation
    
    The code changes introduce a new `Status` enum that represents the status of an order. It includes values such as "Warehouse exit", "On way", "Devolution", "Delivered", and "Delay". The enum provides a method to obtain the status of an order based on a given text. This addition improves the organization and clarity of the codebase.
    
    * feat: update Order class to use Status enum for status representation
    
    * PB-26 refactor/changes some code for enum (#26)
    
    * PB-34 feat/funcionatility with controllers and database (#25)
    
    * PB-38 feat/delete an order in views (#19)
    
    * fix: update pom.xml dependencies for servlet and servlet-api
    
    The code changes in the `pom.xml` file update the dependencies for the servlet and servlet-api. The `jakarta.servlet.jsp.jstl` dependency is removed, and the `javax.servlet-api` dependency is added with version `4.0.1` and scope `provided`. This update aims to align the project with the latest servlet specifications and improve compatibility with the web application.
    
    * fix: add documentation for MongoClientFactory and public class
    
    * feat: add OrderView servlet for deleting orders
    
    The code changes add a new servlet called `OrderView` that handles the deletion of orders. This servlet receives a request with an order ID parameter and uses the `OrderController` and `OrderRepository` to delete the corresponding order from the MongoDB database. The purpose of this addition is to provide functionality for deleting orders in the web application.
    
    * feat: add StringUtils class for UUID validation
    
    The code changes introduce a new utility class called `StringUtils` that provides a method `isUuid` for checking if a string is a valid UUID. This class aims to improve the codebase by centralizing the UUID validation logic and promoting code reusability.
    
    * Pb 36 create view and controller to add orders (#20)
    
    * PB-36 feat:add order
    
    * PB-36 feat: create view controller add orders
    
    ---------
    
    
    
    * PB-34 feat: add display all orders in the table with database (#21)
    
    * PB-35 create view and controller to display orders
    
    * PB-35 feat: connect view controller to display orders
    
    * fix: delete unnecessary import
    
    * fix: modify endpoint name to orders
    
    * refactor: update hrefSection and hrefTitle values in orders.jsp
    
    * feat: Add delete button to orders.jsp
    
    The code changes in `orders.jsp` add a delete button to each order row. This button allows users to delete an order directly from the web page.
    
    * style: change a spacing in one line of jsp
    
    ---------
    
    
    
    
    * PB-38 feat/delete an order in list of jsp (#22)
    
    * refactor: move the scripts of javascript for orders.jsp
    
    * fix: improve UUID validation in StringUtils
    
    * feat: add delete functionality in orders view
    
    * refactor: remove unnecessary code in OrderView
    
    * interface is added to edit orders and send information to the controller (#24)
    
    
    
    ---------
    
    
    
    
    
    * fix: change method to call from document to model on java
    
    * feat: add default status warehouse exit when add an order
    
    * refactor: update select element styles in editorder.jsp
    
    * refactor: update order status display in orders.jsp and cash on delivery
    
    ---------
    
    
    
    
    
    * PB-39 feat:Perform controller method to modify the shipping status of an order (#27)
    
    ---------
    
    
    
    
    
    * PB-39 feat:additions in the add user interface (#29)
    
    * interface to change order status
    
    ---------
    
    Co-authored-by: Bibian Corredor <138538175+BibianCo@users.noreply.github.com>
    Co-authored-by: Jean Valencia <72704457+Jean-Dv@users.noreply.github.com>
    Co-authored-by: Diego Combariza <136619052+DiegoCombariza11@users.noreply.github.com>

--- src/main/java/co/edu/uptc/views/OrderView.java
+++ src/main/java/co/edu/uptc/views/OrderView.java
@@ -103,6 +103,17 @@ public class OrderView extends HttpServlet {
       ServletUtils.forward(req, resp, "/pages/editorder.jsp");
     }
 
+    if (action != null && action.equals("editstate") && id != null) {
+      MongoClient mongoClient =
+          MongoClientFactory.createClient("orderView", "mongodb://localhost:27017");
+      OrderRepository orderRepository = new MongoOrderRepository(mongoClient);
+      OrderController orderController = new OrderController(orderRepository);
+
+      Order order = orderController.getById(id);
+      req.getSession().setAttribute("order", order);
+      ServletUtils.forward(req, resp, "/pages/editstate.jsp");
+    }
+
     ServletUtils.forward(req, resp, "/pages/addorder.jsp");
 
   }
@@ -111,50 +122,74 @@ public class OrderView extends HttpServlet {
   protected void doPut(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
 
-    final String isCashon = req.getParameter("cashonDelivery");
-    String id = req.getParameter("id");
-    String destinationAddress = req.getParameter("destinationAddress");
-    String descriptionAddress = req.getParameter("descriptionAddress");
-    String remitterName = req.getParameter("remitterName");
-    String addresseeName = req.getParameter("addresseeName");
-    String price = req.getParameter("price");
-    String state = req.getParameter("state");
+    String pathInfo = req.getPathInfo();
 
-    if (descriptionAddress == null || destinationAddress == null || remitterName == null
-        || addresseeName == null || price == null || descriptionAddress.isEmpty()
-        || destinationAddress.isEmpty() || remitterName.isEmpty() || addresseeName.isEmpty()
-        || price.isEmpty() || state == null || state.isEmpty()) {
+    if (pathInfo != null && pathInfo.equals("/editstate")) {
+      String state = req.getParameter("state");
+      String id = req.getParameter("id");
 
-      req.setAttribute("id", id);
-      req.setAttribute("action", "edit");
-      req.setAttribute("all_fields_required", "Error: All fields are required");
-      ServletUtils.forward(req, resp, "/pages/editorder.jsp");
-      return;
+      if (state == null || state.isEmpty()) {
+        ServletUtils.forward(req, resp, "/pages/orders.jsp");
+        return;
+      }
+
+      MongoClient mongoClient =
+          MongoClientFactory.createClient("orderView", "mongodb://localhost:27017");
+      OrderRepository orderRepository = new MongoOrderRepository(mongoClient);
+      OrderController orderController = new OrderController(orderRepository);
+
+      Status status = Status.fromString(state);
+      orderController.editStatus(id, status);
+      
+      resp.sendRedirect("/project-programation/orders");
+    } else {
+
+      final String isCashon = req.getParameter("cashonDelivery");
+      String id = req.getParameter("id");
+      String destinationAddress = req.getParameter("destinationAddress");
+      String descriptionAddress = req.getParameter("descriptionAddress");
+      String remitterName = req.getParameter("remitterName");
+      String addresseeName = req.getParameter("addresseeName");
+      String price = req.getParameter("price");
+      String state = req.getParameter("state");
+
+      if (descriptionAddress == null || destinationAddress == null || remitterName == null
+          || addresseeName == null || price == null || descriptionAddress.isEmpty()
+          || destinationAddress.isEmpty() || remitterName.isEmpty() || addresseeName.isEmpty()
+          || price.isEmpty() || state == null || state.isEmpty()) {
+
+        req.setAttribute("id", id);
+        req.setAttribute("action", "edit");
+        req.setAttribute("all_fields_required", "Error: All fields are required");
+        ServletUtils.forward(req, resp, "/pages/editorder.jsp");
+        return;
+      }
+
+      if (!remitterName.matches("^[A-Za-zÁÉÍÓÚáéíóúñÑ ]+$")
+          || !addresseeName.matches("^[A-Za-zÁÉÍÓÚáéíóúñÑ ]+$")) {
+        req.setAttribute("only_letters_are_accepted", "only letters");
+        ServletUtils.forward(req, resp, "/pages/editorder.jsp");
+        return;
+      }
+
+      MongoClient mongoClient =
+          MongoClientFactory.createClient("orderView", "mongodb://localhost:27017");
+      OrderRepository orderRepository = new MongoOrderRepository(mongoClient);
+      OrderController orderController = new OrderController(orderRepository);
+
+      Order order = orderController.getById(id);
+
+      order.setDestinationAddress(destinationAddress);
+      order.setDescription(descriptionAddress);
+      order.setRemitterName(remitterName);
+      order.setAddresseeName(addresseeName);
+      order.setCashonDelivery(isCashon != null);
+      order.setShippingValue(Integer.parseInt(price));
+      order.setStatus(Status.fromString(state));
+      orderController.edit(order);
+
+      resp.sendRedirect("/project-programation/orders");
     }
-
-    if (!remitterName.matches("^[A-Za-zÁÉÍÓÚáéíóúñÑ ]+$")
-        || !addresseeName.matches("^[A-Za-zÁÉÍÓÚáéíóúñÑ ]+$")) {
-      req.setAttribute("only_letters_are_accepted", "only letters");
-      ServletUtils.forward(req, resp, "/pages/editorder.jsp");
-      return;
-    }
-    MongoClient mongoClient =
-        MongoClientFactory.createClient("orderView", "mongodb://localhost:27017");
-    OrderRepository orderRepository = new MongoOrderRepository(mongoClient);
-    OrderController orderController = new OrderController(orderRepository);
-
-    Order order = orderController.getById(id);
-
-    order.setDestinationAddress(destinationAddress);
-    order.setDescription(descriptionAddress);
-    order.setRemitterName(remitterName);
-    order.setAddresseeName(addresseeName);
-    order.setCashonDelivery(isCashon != null ? true : false);
-    order.setShippingValue(Integer.parseInt(price));
-    order.setStatus(Status.fromString(state));
-    orderController.edit(order);
-    resp.sendRedirect("/project-programation/orders");
-    return;
   }
 
   @Override

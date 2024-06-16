c4799af167c176775eac64762d741c8b842f7a59
Bibian Corredor <bibian.corredor@gmail.com>
2024-06-15 23:16:08 -0500

    PB-45 feat: add AggregateRoot extension and fromSettings method

--- src/main/java/co/edu/uptc/model/Settings.java
+++ src/main/java/co/edu/uptc/model/Settings.java
@@ -1,15 +1,34 @@
 package co.edu.uptc.model;
 
+import java.util.UUID;
+import org.bson.Document;
+
 /**
  * Class that represents a settings.
  */
-public class Settings {
+public class Settings extends AggregateRoot {
 
   private String sourceAddress;
 
   public Settings() {}
 
+  /**
+   * Parameterized constructor.
+   *
+   * @param sourceAddress The address from which the package will be sent.
+   */
   public Settings(String sourceAddress) {
+    this.sourceAddress = sourceAddress;
+  }
+
+  /**
+   * Parameterized constructor.
+   *
+   * @param id The unique identifier for the settings.
+   * @param sourceAddress The address from which the package will be sent.
+   */
+  public Settings(UUID id, String sourceAddress) {
+    super(id);
     this.sourceAddress = sourceAddress;
   }
 
@@ -23,6 +42,26 @@ public class Settings {
 
   @Override
   public String toString() {
-    return "Settings [sourceAddress=" + sourceAddress + "]";
+    return "Settings [id=" + id + "sourceAddress=" + sourceAddress + "]";
+  }
+
+  @Override
+  public Document toDocument() {
+    Document document = new Document();
+    document.append("_id", this.id.toString());
+    document.append("sourceAddress", this.sourceAddress);
+    return document;
+  }
+
+  /**
+   * Method to convert a document to an settings object.
+   *
+   * @param document Document to convert.
+   * @return The settings object.
+   */
+  public static Settings fromSettings(Document document) {
+    UUID id = UUID.fromString(document.getString("_id"));
+    String sourceAddress = document.getString("sourceAddress");
+    return new Settings(id, sourceAddress);
   }
 }
